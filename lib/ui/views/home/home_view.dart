72a43212b7bafb451f6a3510f3c6536828d22229
swapnil andhare <swapnilandhare608@gmail.com>
2022-07-05 11:58:20 +0530

    Initial commit

---
+++ lib/ui/views/home/home_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+import 'home_viewmodel.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<HomeViewModel>.reactive(
+      builder: (context, model, child) => Scaffold(
+        appBar: AppBar(
+          title: Text("Counter App"),
+        ),
+        body: Text("Current count is ${model.count}"),
+      ),
+      viewModelBuilder: () => HomeViewModel(),
+    );
+  }
+}
---
+++ lib/ui/views/home/home_viewmodel.dart
@@ -0,0 +1,6 @@
+import 'package:stacked/stacked.dart';
+
+class HomeViewModel extends BaseViewModel {
+  int _count = 0;
+  int get count => _count;
+}
